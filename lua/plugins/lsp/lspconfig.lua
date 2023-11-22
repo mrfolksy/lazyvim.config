0a12e671a462a2db79cf047a0aedd24745a948a2
Your Name <you@example.com>
2023-11-22 08:34:20 +0000

    updates

---
+++ lua/plugins/lsp/lspconfig.lua
@@ -0,0 +1,3 @@
+return {
+  -- TODO - fill this in
+}
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -2,10 +2,13 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
+      "css-lsp",
+      "html-lsp",
       "stylua",
       "shfmt",
       "powershell-editor-services",
       "pyright",
+      "graphql-language-server-cli",
     },
   },
 }
