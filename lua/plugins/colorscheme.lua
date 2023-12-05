995858ced9150e8944a57a477a34ff464d2babe1
christopher.gell@bp.com <christopher.gell@bp.com>
2023-12-05 21:12:59 +0000

    added new colorschemes and dap lazyvim extras

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,29 @@
 return {
   {
+    {
+      "ellisonleao/gruvbox.nvim",
+      name = "gruvbox",
+      lazy = false,
+    },
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = false,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+  },
+  {
     "bluz71/vim-nightfly-colors",
     name = "nightfly",
     lazy = false,
   },
 
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "kanagawa",
     },
   },
 }
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -4,6 +4,29 @@ return {
     opts = {
       servers = {
         tailwindcss = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                version = "Lua 5.1",
+                path = {
+                  "?.lua",
+                  "?/init.lua",
+                  vim.fn.expand("~/.luarocks/share/lua/5.1/?.lua"),
+                  vim.fn.expand("~/.luarocks/share/lua/5.1/?/init.lua"),
+                  "/usr/share/5.1/?.lua",
+                  "/usr/share/lua/5.1/?/init.lua",
+                },
+              },
+              workspace = {
+                library = {
+                  vim.fn.expand("~/.luarocks/share/lua/5.1"),
+                  "/usr/share/lua/5.1",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
