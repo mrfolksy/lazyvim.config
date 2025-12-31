5542643b7e1e14d8b75138d88a9e412b290c5a6b
chris gell <chris@MacBookPro.localdomain>
2025-12-31 09:18:54 +0000

    updated lazy.lua to use new plugin names

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.dotnet" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
